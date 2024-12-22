f75cbff4c97746bb2d34ceb5805472e72ac28207
Armin <armin.raaaaad@gmail.com>
2024-12-22 14:22:28 +0100

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import hamburgerIcon from "../../assets/svg/hamburger-menu-icon.svg";
+import notificationIcon from "../../assets/svg/notification-icon.svg";
 import youtubeBoxIcon from "../../assets/youtube_assets/youtube-box-icon.png";
+
 import "./Navbar.css";
 
 import SearchBar from "../SearchBar/SearchBar";
 
 export default function Navbar() {
-
   return (
     <div id="container">
       <div className="start">
@@ -17,16 +17,33 @@ export default function Navbar() {
           />
         </button>
 
-        <img className="youtube-box-logo" src={youtubeBoxIcon} alt="" />
-        <span className="youtube-text">YouTube</span>
-        <span className="country-tag">SE</span>
+        <div className="youtube-logo-container">
+          <img className="youtube-box-logo" src={youtubeBoxIcon} alt="" />
+          <p className="youtube-text">YouTube</p>
+          <p className="country-tag">SE</p>
+        </div>
       </div>
 
       <div className="center">
-        <SearchBar />
+        <SearchBar /> {/*TODO: search logo popup when clicked*/}
       </div>
 
-      <div className="end">end</div>
+      <div className="end">
+        <div className="create-btn-div">
+          <button>
+            <span>+</span>
+            <p>Skapa</p>
+          </button>
+        </div>
+
+        <div className="notification-btn-div">
+          <button>
+            <img src={notificationIcon} alt="notificationIcon" />
+          </button>
+        </div>
+
+        <div className="profile-btn">(profile)</div>
+      </div>
     </div>
   );
 }
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import keyboardIcon from "../../assets/youtube_assets/keyboard.png";
+import microphoneIcon from "../../assets/youtube_assets/white-microphone-icon.png";
+import searchSVG from "../../assets/svg/white-search-icon.svg";
 import "./SearchBar.css";
 
 export default function SearchBar() {
@@ -8,15 +11,39 @@ export default function SearchBar() {
     setSearchQuery(e.target.value);
   };
 
-  return(
+  return (
     <div className="container">
-      <input className="search-input"
-        type="text"
-        placeholder="Sök"
-        value={searchQuery}
-        onChange={handleInputChange}
+      <div className="input-wrapper">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Sök"
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
 
-      />
+        <button className="keyboard-button">
+          <img
+            className="keyboard-icon"
+            src={keyboardIcon}
+            alt="Keyboard Icon"
+          />
+        </button>
+
+        <button className="search-button">
+          <img
+            className="search-icon"
+            src={searchSVG}
+            alt="Search Icon missing"
+          />
+        </button>
+      </div>
+
+      <div className="microphone-div">
+        <button className="microphone-icon-btn">
+          <img src={microphoneIcon} alt="" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
